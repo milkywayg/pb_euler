ece73cdb737bf975600d0d6577dce6f5b3530562
milkywayg <michael.ganem@gmail.com>
2020-11-25 23:00:08 +0000

    pb5 wip

--- pb5/pb5.ml
+++ pb5/pb5.ml
@@ -4,15 +4,16 @@ let rec decomp_in_primes x primes lst_dec =
     match primes with
     | [] -> lst_dec
     | p::rem_p -> 
+            let tmp_lst=
+                if x mod p=0 then (* p divise x, lst_dec cannot be empty *)
+                    match List.hd lst_dec with
+                    | p,n -> (p,n+1)::(List.tl lst_dec)
+                    | _   -> (p,1)::lst_dec
+                else lst_dec in  (* p does not divise x *) 
             let lst_dec_rem = decomp_in_primes x rem_p lst_dec in 
-            if x mod p=0 then (* p divise x *)
-                    if lst_dec=[] then (p,1)::lst_dec_rem
-                    else
-                        match List.hd lst_dec with
-                        | p,n -> (p,n+1)::List.tl@lst_dec_rem
-                        | _   -> (p,1)::List.tl@lst_dec_rem
-            else (* p does not divise x *)
-                lst_dec_rem;;
+            lst_dec_rem@tmp_lst;;
+
+
 
 
 
