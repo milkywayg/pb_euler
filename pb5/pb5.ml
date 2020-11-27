bc51a931a6a8afbe95ac1508caab637e58a9179e
milkywayg <michael.ganem@gmail.com>
2020-11-27 00:07:43 +0000

    pb5 wip

--- pb5/pb5.ml
+++ pb5/pb5.ml
@@ -1,21 +1,81 @@
-let primes=[2;3;5;7;11;13;17;19];
+open Batteries
 
-let rec decomp_in_primes x primes lst_dec = 
-    match primes with
+let primes=[2;3;5;7;11;13;17;19]
+
+let rec print_list = function
+[] -> ()
+| e::l -> print_int e ; print_string " " ; print_list l
+
+let rec decomp_in_primes (x:int) prim lst_dec = 
+    match prim with
     | [] -> lst_dec
     | p::rem_p -> 
-            let tmp_lst=
-                if x mod p=0 then (* p divise x, lst_dec cannot be empty *)
-                    match List.hd lst_dec with
-                    | p,n -> (p,n+1)::(List.tl lst_dec)
-                    | _   -> (p,1)::lst_dec
-                else lst_dec in  (* p does not divise x *) 
-            let lst_dec_rem = decomp_in_primes x rem_p lst_dec in 
-            lst_dec_rem@tmp_lst;;
+            let tmp_lst, tmp_x, tmp_prim =
+                if lst_dec=[] then
+                    if x mod p=0 then
+                        [(p,1)], x/p, prim
+                    else [], x, rem_p
+                else if x mod p=0 then (* p divise x, lst_dec cannot be empty *)
+                    (* print_list lst_dec;  *)
+                    match (List.hd lst_dec) with
+                    | (aa,n) -> if aa=p then (p,n+1)::(List.tl lst_dec), x/p, prim
+                               else  (p,1)::lst_dec, x/p, prim
+                else lst_dec, x, rem_p in  (* p does not divise x *) 
+            let lst_dec_rem = decomp_in_primes tmp_x tmp_prim tmp_lst  in 
+            lst_dec_rem 
 
 
+let rec decomp_prime_list lst =
+    match lst with
+    | []   -> []
+    | h::t -> (h, decomp_in_primes h primes [])::decomp_prime_list t
+
+(* let rec range a b = *)
+let lst_0_20 = List.of_enum (1--20)
+
+let my_decomp= decomp_prime_list lst_0_20
+
+(* print_list my_decomp *)
+
+(* assumes a>b *)
+let rec gcd_tmp a b=
+    match b with
+    |0 -> a
+    |_ -> gcd_tmp b (a mod b)
+
+let gcd a b =
+    if a>b then gcd_tmp a b
+    else gcd_tmp b a
 
 
+let rec gcd_l lst=
+    if List.length lst = 2 then 
+        match lst with
+        |a::b::[] -> gcd a b
+        |_  -> 0
+    else 
+        match lst with
+        |h::t -> gcd h (gcd_l t)
+        |_ -> 0
+
+let rec prod_l lst =
+    match lst with
+    | a::[] -> a 
+    | h::tl -> h*prod_l tl
+    | [] -> 1
+
+(* this function is wrong *)
+let rec lcm_l lst =
+    (prod_l lst)/Int.pow (gcd_l lst) (List.length lst-1)
+(*     if List.length lst = 2 then  *)
+(*     match lst with *)
+(*     |a::b::[] -> (a*b)/(gcd a b) *)
+(*     |_  -> 1 *)
+(* else  *)
+(*     match lst with *)
+(*     |h::t -> prod_l gcd h (gcd_l t) *)
+(*     |_ -> 1 *)
+(*  *)
 
 
 
