a4574ae2ec5bdcba8033b3a25c053f1327d9b1ed
milkywayg <michael.ganem@gmail.com>
2020-12-10 22:19:24 +0000

    ff

---
+++ pb6/pb6.ml
@@ -0,0 +1,17 @@
+(* #require "batteries" *)
+open Batteries
+open BatInt
+
+
+let rec sum_of_square n_max = 
+   match n_max with
+   |1 -> 1
+   |n -> n*n+(sum_of_square (n-1))
+
+let diff_of_sums n_max=
+    (n_max*(n_max+1)/2)**2-sum_of_square n_max
+
+
+diff_of_sums 100;;
+
+
