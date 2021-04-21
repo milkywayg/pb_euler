7352cd29e346258bd8b285c10d3aea9a12ae5ce5
milkywayg <michael.ganem@gmail.com>
2021-04-21 19:37:52 +0000

    dd

--- pb10/pb10.ml
+++ pb10/pb10.ml
@@ -95,9 +95,31 @@ let sum_prime_till2 p_max =
         else p_list, p_sum in
     build_p_list 3 [2] 2;;
 
+let int_sqrt num =
+    int_of_float(sqrt(float_of_int (num)))
+
+let rec is_prime n_num p_list =
+    match p_list with
+    |[] -> false
+    |h::tl -> if h > int_sqrt(n_num) then true
+              else if (n_num mod h=0) then false
+              else is_prime n_num tl
+
+let sum_prime_till3 p_max = 
+    let rec build_p_list n_num p_list p_sum=
+        if (n_num <p_max) then 
+            if (n_num mod 2 =0) then build_p_list (n_num+1) p_list p_sum
+            else (*n_num odd *)
+                if (is_prime n_num p_list) then 
+                    let tmp_list, tmp_p_sum = build_p_list (n_num+2) (p_list@[n_num]) (n_num+p_sum) in 
+                    tmp_list, tmp_p_sum
+                else build_p_list (n_num+2) p_list p_sum
+        else p_list, p_sum in
+    build_p_list 8 [2;3;5;7] 17;;
+
 
 let ()= 
-    let tmp_l, ps=(sum_prime_till2 2000000) in
+    let tmp_l, ps=(sum_prime_till3 2000000) in
     print_int ps;;
 
 
